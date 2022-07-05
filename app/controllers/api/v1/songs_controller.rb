72438ace9c72650e1c082a562682d465f2032ee0
nyohoo <nyohoo2013@gmail.com>
2022-07-05 12:42:59 +0900

    fix: アクション名を変更

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::SongsController < ApplicationController
   require 'rspotify'
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
 
-  def index
-    @musics = RSpotify::Track.search(params[:query], limit: 10, market: 'JP')
+  def search
+    @musics = RSpotify::Track.search(params[:query], limit: 20)
     render json: @musics
   end
 end
