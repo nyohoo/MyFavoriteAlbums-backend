98103441f8e536efde7c1b05ed8ff33e876f764b
nyohoo <nyohoo2013@gmail.com>
2022-07-27 08:37:58 +0900

    add: post_albumsの処理を仮で実装

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -4,7 +4,51 @@ class Api::V1::SongsController < ApplicationController
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
 
   def search
-    @musics = RSpotify::Track.search(params[:query], limit: 28, market: 'JP')
-    render json: @musics
+    result = RSpotify::Album.search(params[:query], limit: 48, market: 'JP')
+    render json: result
   end
+
+  def post_albums
+    albums = params[:albums]
+    hash_tag = params[:hash_tag]
+
+    # 画像を取得する
+    @album_images = []
+    @album_ids = []
+    albums.each do |album|
+      @album_images.push(album["images"][0]["url"])
+      @album_ids.push(album["id"])
+    end
+
+    logger.debug("------------@album_images--------------------")
+    logger.debug(@album_images)
+    logger.debug("---------------------------------------------")
+
+    logger.debug("------------@album_ids-----------------------")
+    logger.debug(@album_ids)
+    logger.debug("---------------------------------------------")
+
+    @albums = RSpotify::Album.find(@album_ids)
+    logger.debug("------------@albumsの中身！！---------------")
+    logger.debug(@albums)
+    logger.debug("--------------------------------------------")
+
+      @albums.each do |album|
+        @album = album
+        logger.debug("------------Spotifyで検索してみる------------")
+        logger.debug(@album)
+        logger.debug("-------------------------------------------")
+      end
+
+    logger.debug("------------CurrentUser---------------")
+    logger.debug(current_api_v1_user.id)
+
+  render json: @albums
+
+  end
+
+  def get_albums
+
+  end
+
 end
