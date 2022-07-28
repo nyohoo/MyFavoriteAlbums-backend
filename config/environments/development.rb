1f7d2abc1db8e83d57be72fbc000cfb4e62f9457
nyohoo <nyohoo2013@gmail.com>
2022-07-29 00:15:54 +0900

    add: ActiveStorage導入でNameError: uninitialized constant ::Analyzableエラーが発生した対処

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # ActiveStorage導入でNameError: uninitialized constant ::Analyzableエラーが発生した対処
+  config.autoloader = :classic
 end
