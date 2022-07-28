2144504315e41f4360f2a5c4557f9613d7c060f4
nyohoo <nyohoo2013@gmail.com>
2022-07-28 21:57:07 +0900

    add: post.rbにactivestorageの設定を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :albums, dependent: :destroy
   has_many :users, through: :likes
+  has_one_attached :image
+
 end
