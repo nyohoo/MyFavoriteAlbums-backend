6a95855bb5aa521543f7852424b5dccad98fdf66
nyohoo <nyohoo2013@gmail.com>
2022-08-01 23:57:30 +0900

    add: showアクション作成

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,28 +1,42 @@
 class Api::V1::PostsController < ApplicationController
   include Rails.application.routes.url_helpers
+  require 'rspotify'
+  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
 
   def show
-    @post = Post.find(params[:id])
-    render json: @post, include: [:user, :likes, :albums, :image]
+    post = Post.find_by(uuid: params[:uuid])
+    # image = post.image.service_url
+    album_ids = post.albums.pluck(:album_id)
+    albums = RSpotify::Album.find(album_ids)
+    dates = []
+    albums.each do |album|
+      if album.release_date.present?
+        dates << album.release_date.split('-')[0]
+      else
+        dates << ""
+      end
+    end
+    render json: { user: post.user, albums: albums, hash_tag: post.hash_tag, dates: dates }
   end
 
   def create
     if api_v1_user_signed_in?
       # 一意の画像パス生成のためuidを生成
-      uid = SecureRandom.hex(3)
+      uuid = SecureRandom.hex(8)
       # 画像を3×3のタイルに加工
       MiniMagick::Tool::Montage.new do |montage|
         params[:image_paths].each { |image| montage << image }
         montage.geometry "640x640+0+0"
         montage.tile "3x3"
-        montage << "tmp/images/#{uid}.jpg"
+        montage << "tmp/images/#{uuid}.jpg"
       end
       # 画像パスの取得
-      image_path = "tmp/images/#{uid}.jpg"
+      image_path = "tmp/images/#{uuid}.jpg"
       # Postモデルに新規投稿
       tmp = Post.new(
         user_id: current_api_v1_user.id,
-        hash_tag: params[:hash_tag]
+        hash_tag: params[:hash_tag],
+        uuid: uuid
       )
       post = tmp.image.attach(io: File.open(image_path),
                               filename: File.basename(image_path),
@@ -38,7 +52,7 @@ class Api::V1::PostsController < ApplicationController
             post_id: post.record.id
           )
         end
-        render json: post.record.id
+        render json: post.record.uuid
       else
         render json: { error: post.errors.full_messages }, status: :unprocessable_entity
       end
