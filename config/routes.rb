a2f5b63cbe9ced6b0e1409fd8922cfd031bbbaa0
nyohoo <nyohoo2013@gmail.com>
2022-08-03 15:08:51 +0900

    comment for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       namespace :auth do
-        resources :sessions, only: %i[index]
+        # resources :sessions, only: %i[index]
       end
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         omniauth_callbacks: 'overrides/omniauth_callbacks',
