af2f49ed3c797ebb6103fa92b671dab521cd8f17
nyohoo <nyohoo2013@gmail.com>
2022-08-03 15:46:10 +0900

    fix: herokuデプロイ コントローラーのパスを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      # namespace :auth do
-      #   resources :sessions, only: %i[index]
-      # end
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         omniauth_callbacks: 'overrides/omniauth_callbacks',
       }
