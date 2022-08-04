d11fa8a20495fef08a64598fff9a96fee11f7c5f
nyohoo <nyohoo2013@gmail.com>
2022-08-05 01:47:42 +0900

    fix: コールバックURLの記述を修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.silence_get_warning = true
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_API_CONSUMER_KEY'], ENV['TWITTER_API_CONSUMER_SECRET'], ENV['TWITTER_API_CALLBACK_URL']
+  provider :twitter, ENV['TWITTER_API_CONSUMER_KEY'], ENV['TWITTER_API_CONSUMER_SECRET'], callback_url: ENV['TWITTER_API_CALLBACK_URL']
 end 
