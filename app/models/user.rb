802c583181e38852e835ee5c98d9671521c9b353
nyohoo <nyohoo2013@gmail.com>
2022-08-06 06:55:22 +0900

    add: アクセストークンをDBに保存する際に暗号化

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,20 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
+
+  before_save :encrypt_access_token
+
+  # 返すユーザー情報のフィルタ
+  def token_validation_response
+    as_json(only: [:id, :uid, :name, :nickname, :image])
+  end
+
+  # access_tokenとaccess_token_secretを暗号化して保存する
+  def encrypt_access_token
+    key_len = ActiveSupport::MessageEncryptor.key_len
+    secret = Rails.application.key_generator.generate_key('salt', key_len)
+    crypt = ActiveSupport::MessageEncryptor.new(secret)
+    self.access_token = crypt.encrypt_and_sign(access_token)
+    self.access_token_secret = crypt.encrypt_and_sign(access_token_secret)
+  end
 end
