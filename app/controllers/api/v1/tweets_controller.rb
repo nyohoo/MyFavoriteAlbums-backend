5c71dcc20cc9959913a4c3d7cf8e280b515f1e41
nyohoo <nyohoo2013@gmail.com>
2022-08-06 19:10:25 +0900

    add: テストで一旦ブランチ切り替え

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::TweetsController < ApplicationController
   require 'twitter'
+  require 'open-uri'
   def create
-    post = Post.find_by(params[:post_uuid])
+    post = Post.find_by(uuid: params[:post_uuid])
 
     # ツイートを投稿するためにユーザーのアクセストークンを設定したclientを生成
     client = Twitter::REST::Client.new do |config|
@@ -12,7 +13,15 @@ class Api::V1::TweetsController < ApplicationController
     end
 
     # S3のURLを取得
+    
     post_image = post.image.service_url
+    
+    # ツイートの本文を作成
+    text = "テストです！！" +  "\n" + "#{post.hash_tag}"
+
+    # ツイッターAPIのリファレンスに従い、画像のチャンクを作成
+    # base_file = Base64.decode64(post_image)
+
     # S3のURLでは長すぎるので、postのuuidを使って短くして
     # バイナリ形式で一時保存してから投稿する
     open("./tmp/#{post.uuid}", 'w+b') do |output|
@@ -22,8 +31,22 @@ class Api::V1::TweetsController < ApplicationController
         post_image = output.path
       end
     end
-    # ツイッターにpost_imageの画像と、params[:text]の内容を含めてツイートする
-    client.update_with_media(params[:text], open(post_image))
+
+    file = post_image
+    # S3の画像をjpg形式に変換してから投稿する
+
+    init_request = Twitter::REST::Request.new(client, :post, "https://upload.twitter.com/1.1/media/upload.json", command: 'INIT', total_bytes: base_file.size).perform
+    media_id = init_request[:media_id]
+
+    Twitter::REST::Request.new(client, :post, "https://upload.twitter.com/1.1/media/upload.json", command: 'APPEND', media_id: media_id, media: file, segment_index: 0).perform
+
+    Twitter::REST::Request.new(client, :post, "https://upload.twitter.com/1.1/media/upload.json", command: 'STATUS', media_id: media_id, media: file).perform
+
+    Twitter::REST::Request.new(client, :post, "https://upload.twitter.com/1.1/media/upload.json", command: 'FINALIZE',media_id: media_id).perform
+
+    # テキスト・URL・画像を投稿
+    Twitter::REST::Request.new(client, :post, "https://api.twitter.com/1.1/statuses/update.json", status: params[:text], attachment_url: params[:url], media_ids: media_id).perform
+
 
     render json: { message: "ツイートが完了しました" }
   end
