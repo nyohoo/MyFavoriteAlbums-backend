f6658caae5691cf757a7875559fc8d73830ac257
nyohoo <nyohoo2013@gmail.com>
2022-08-07 23:34:52 +0900

    add: force_loginをfalseにする

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,8 @@ OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.silence_get_warning = true
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_API_CONSUMER_KEY'], ENV['TWITTER_API_CONSUMER_SECRET'],
-  {
+  { 
+    force_login: false,
     secure_image_url: true,
     callback_url: ENV['TWITTER_API_CALLBACK_URL'],
     :image_size => 'bigger'
