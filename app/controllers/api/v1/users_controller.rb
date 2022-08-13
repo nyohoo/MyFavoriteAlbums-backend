e7ba84197d417b44ce52115c67419334e169585a
nyohoo <nyohoo2013@gmail.com>
2022-08-14 05:31:33 +0900

    add: userに紐づくlikesとpostsをアクション切り離し

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,42 @@
 class Api::V1::UsersController < ApplicationController
-  def show
-    # userをuidでpostsを含めて取得
+  def show_users_post
+    # kaminariで無限スクロール。userのpostsを取得。
     user = User.includes(:posts).find_by(uid: params[:uid])
-    
-    posts = []
-    # userに紐づくpostごとにフロントエンドで使用するデータを生成
+    posts = user.posts.order("created_at DESC").page(params[:page]).per(5)
+    pagenation = resources_with_pagination(posts)
+
+    results = []
+
+    # userに紐づくpostをフロントエンドで使用する形式に変換
     user.posts.each do |post|
       # post.created_atを年月日のフォーマットに変更
       created_at = post.created_at.strftime("%Y年%m月%d日")
-      posts << { post_uuid: post.uuid, created_at: created_at, hash_tag: post.hash_tag, image_path: post.image.service_url }
+      results << { post_uuid: post.uuid,
+                  created_at: created_at,
+                  hash_tag: post.hash_tag,
+                  image_path: post.image.service_url }
     end
 
-    render json: { user: user, posts: posts }
+    render json: { user: user, posts: results, kaminari: pagenation }
+  end
+
+  def show_users_like
+    user = User.includes(:posts, :likes).find_by(uid: params[:uid])
+    posts = user.posts.order("created_at DESC").page(params[:page]).per(5)
+    pagenation = resources_with_pagination(posts)
+
+    results = []
+
+    # userに紐づくlike_postsをフロントエンドで使用する形式に変換
+    user.like_posts.each do |post|
+      created_at = post.created_at.strftime("%Y年%m月%d日")
+      results << { user: post.user, 
+                  post_uuid: post.uuid,
+                  created_at: created_at, 
+                  hash_tag: post.hash_tag, 
+                  image_path: post.image.service_url }
+    end
+
+    render json: { user: user, likes: results, kaminari: pagenation }
   end
 end
