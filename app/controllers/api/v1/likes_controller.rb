d2d9a138ed8cfe32cdec5bd1a998440464d57955
nyohoo <nyohoo2013@gmail.com>
2022-08-14 05:33:27 +0900

    add: いいね機能

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::LikesController < ApplicationController
   before_action :authenticate_api_v1_user!
   def create
-    current_api_v1_user.likes.create(post_id: params[:post_id])
-    head :created
+    # すでにいいねしているかを確認
+    if current_api_v1_user.likes.find_by(post_id: params[:id]).nil?
+      # いいねしていない場合は、いいねを登録する   
+      current_api_v1_user.likes.create(post_id: params[:id])
+      render json: { message: 'いいねしました' }
+    else
+      render json: { message: 'すでにいいねしています' }
+    end
   end
 
   def destroy
-    current_api_v1_user.likes.find_by(post_id: params[:post_id]).destroy
+    current_api_v1_user.likes.find_by(post_id: params[:id]).destroy
+    render json: { message: 'いいねを取り消しました' }
   end
 end
