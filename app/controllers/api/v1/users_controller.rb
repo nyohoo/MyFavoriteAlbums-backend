21969518187055c4df131d697ef4e422491c14f6
nyohoo <nyohoo2013@gmail.com>
2022-08-16 14:03:20 +0900

    fix: 作業を一旦戻し

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-  include Rails.application.routes.url_helpers #url_forを利用するために、rails_helperをincludeする
   def show_user
     user = User.find_by(uid: params[:uid])
     render json: { user: user }
@@ -14,11 +13,10 @@ class Api::V1::UsersController < ApplicationController
     posts.each do |post|
       # post.created_atを年月日のフォーマットに変更
       created_at = post.created_at.strftime("%Y年%m月%d日")
-      image_path = url_for(post.image)
       results << { post_uuid: post.uuid,
                   created_at: created_at,
                   hash_tag: post.hash_tag,
-                  image_path: image_path }
+                  image_path: post.image.service_url }
     end
 
     render json: { posts: results }
@@ -32,12 +30,11 @@ class Api::V1::UsersController < ApplicationController
     # userに紐づくlike_postsをフロントエンドで使用する形式に変換
     like_posts.each do |like_post|
       created_at = like_post.post.created_at.strftime("%Y年%m月%d日")
-      image_path = url_for(like_post.post.image)
       results << { user: like_post.post.user, 
                   post_uuid: like_post.post.uuid,
                   created_at: created_at, 
                   hash_tag: like_post.post.hash_tag, 
-                  image_path: image_path }
+                  image_path: like_post.post.image.service_url }
     end
 
     render json: { likes: results }
