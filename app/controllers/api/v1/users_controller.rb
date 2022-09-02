1bca5bed2d9ca6dde85de2b085d0834a4ef54dfb
nyohoo <nyohoo2013@gmail.com>
2022-09-02 14:20:23 +0900

    fix: user_controllerをシリアライザーを利用してリファクタ

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-  include Rails.application.routes.url_helpers #url_forを利用するために、rails_helperをincludeする
   require 'rspotify'
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
   
@@ -17,37 +16,24 @@ class Api::V1::UsersController < ApplicationController
   def show_user_likes
     # kaminariで無限スクロール。userのlikesを取得。nullの場合は空の配列を返す。
     like_posts = User.find_by(uid: params[:uid]).likes.page(params[:page]).per(5)
-    results = []
 
-    # userに紐づくlike_postsをフロントエンドで使用する形式に変換
-    like_posts.each do |like_post|
-      created_at = like_post.post.created_at.strftime("%Y年%m月%d日")
-      image_path = url_for(like_post.post.image)
-      results << { user: like_post.post.user,
-                  post_id: like_post.post.id,
-                  post_uuid: like_post.post.uuid,
-                  created_at: created_at, 
-                  hash_tag: like_post.post.hash_tag, 
-                  image_path: image_path }
-    end
-
-    render json: { likes: results }
+    render json: like_posts, each_serializer: LikeSerializer
   end
 
   def show_user_bookmarks
+    # ログイン中の場合は、ログイン中のユーザーのブックマーク情報を取得する
     if api_v1_user_signed_in?
       current_user_bookmarks = current_api_v1_user.bookmarks.select("spotify_album_id").map(&:spotify_album_id)
     else
       current_user_bookmarks = nil
     end
 
-    # kaminariで無限スクロール。userのlikesを取得。nullの場合は空の配列を返す。
-    bookmarks = User.find_by(uid: params[:uid]).bookmarks.select("spotify_album_id").page(params[:page]).per(10).map(&:spotify_album_id)
+    # kaminariで無限スクロール。userのbookmarksを取得。nullの場合は空の配列を返す。
+    bookmark_album_ids = User.find_by(uid: params[:uid]).bookmarks.select("spotify_album_id").page(params[:page]).per(10).map(&:spotify_album_id)
 
-    if bookmarks.present?
+    if bookmark_album_ids.present?
+      albums = RSpotify::Album.find(bookmark_album_ids)
       results = []
-
-      albums = RSpotify::Album.find(bookmarks)
       # リリースデータを年のみのフォーマットに整える
       dates = []
       albums.each do |album|
@@ -67,7 +53,7 @@ class Api::V1::UsersController < ApplicationController
                     albumImagePath: album.images[0]['url'],
                     albumReleaseDate: date }
       end
-      render json: { bookmarks: results, currentUserBookmarks: current_user_bookmarks }  
+      render json: results, current_user_bookmarks: current_user_bookmarks
 
     else 
       render json: { bookmarks: [] }
